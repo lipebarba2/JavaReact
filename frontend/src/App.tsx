2dba7d4be01fdd38d3a82ef7444b3ea6eb69b8d5
lipebarba2 <felipecinemkt@gmail.com>
2024-10-02 15:52:56 -0300

    feat Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
-
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Pagina principal</h1>
+    <div>
+        <Navbar />
     </div>
   );
 }
