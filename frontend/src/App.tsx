50a720bbae3a95cbf60804c72d2f23a6715fa854
lipebarba2 <felipecinemkt@gmail.com>
2024-10-02 16:10:13 -0300

    feat routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import Navbar from './components/Navbar';
-
-function App() {
-  return (
-    <div>
+import {
+    BrowserRouter,
+    Routes,
+    Route
+  } from "react-router-dom";
+  import Listing from "pages/Listing";
+  import Form from "pages/Form";
+  import Navbar from "components/Navbar";
+  
+  function App() {
+    return (
+      <BrowserRouter>
         <Navbar />
-    </div>
-  );
-}
-
-export default App;
+        <Routes>
+          <Route path="/" element={<Listing />} />
+          <Route path="/form">
+            <Route path=":movieId" element={<Form />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    );
+  }
+  
+  export default App;
--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -4,21 +4,21 @@ import './styles.css'
 function Navbar() {
 
     return (
-        <header>
-        <nav className='container'>
-          <div className='dsmovie-nav-content'>
-            <h1>DSMovie</h1>
-            <a href="https://github.com/lipebarba2">
-              <div className='dsmovie-contact-container'>
-                <GithubIcon />
-                <p className='dsmovie-contact-link'>/DevSuperior</p>
-              </div>
-              
-            </a>
-          </div>
-        </nav>
-      </header>
-    )
+            <header>
+                <nav className='container'>
+                <div className='dsmovie-nav-content'>
+                    <h1>DSMovie</h1>
+                    <a href="https://github.com/lipebarba2">
+                    <div className='dsmovie-contact-container'>
+                        <GithubIcon />
+                        <p className='dsmovie-contact-link'>/DevSuperior</p>
+                    </div>
+                    
+                    </a>
+                </div>
+                </nav>
+            </header>
+        )
 
 }
 
