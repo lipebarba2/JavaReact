31e404d082562b7067e721600b0587a6897afa75
lipebarba2 <felipecinemkt@gmail.com>
2024-10-07 20:01:52 -0300

    review

--- backend/src/main/java/com/devsuperior/dsmovie/DsmovieApplication.java
+++ backend/src/main/java/com/devsuperior/dsmovie/DsmovieApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 public class  DsmovieApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DsmovieApplication.class, args);
+		SpringApplication.run(DsmovieApplication.class, args); 
 	}
 
 }
